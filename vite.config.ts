b21bf84973567ac2b33b989de39e6b6aa6631a3d
namtp42 <tpnam42@lqdtu.edu.vn>
2026-01-18 22:10:32 +0700

    doimoilan03

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
-    }
+    },
   },
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  }
 });
